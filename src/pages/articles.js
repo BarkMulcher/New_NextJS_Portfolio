ba179f3139c69bb9040c37078f3fcf3378a761d2
burpmonster <lucas.c.haskell@gmail.com>
2026-05-10 18:05:31 -0400

    updating portfolio articles to less immature content

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -100,14 +100,14 @@ const Articles = () => {
                     <AnimatedText text='Articles' className='!text-8xl !leading-tight mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8' />
                     <ul className='grid grid-cols-2 gap-16 lg:gap-8 md:grid-cols-1 md:gap-y-16'>
                         <FeaturedArticle
-                        title='How to Increase Your IQ by Eating Gifted Children'
+                        title='How to Increase Your IQ'
                         summary='read title'
                         time='9 min read'
                         link='/'
                         img={article1}
                         />
                         <FeaturedArticle
-                        title='How to Increase Your IQ by Eating Gifted Children'
+                        title='How to Increase Your IQ'
                         summary='read title'
                         time='9 min read'
                         link='/'
